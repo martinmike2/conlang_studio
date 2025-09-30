13d2f42f5601ae06093bfea06ea52057eb90cc75
Michael Martin <martinmike2@gmail.com>
2025-09-30 19:29:16 +0000

    remove .next from tracking, update lock file

--- apps/web/.next/static/chunks/pages/_error-b5305471fddbf85c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{5315:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(5478)}])}},_=>{_.O(0,[636,593,792],()=>_(_.s=5315)),_N_E=_.O()}]);
